747160034897723fda45feceab3268bd546a4724
dhyeypatel188 <pateldhyey649@gmail.com>
2026-09-22 00:08:54 +0530

    cores error

--- app/main.py
+++ app/main.py
@@ -72,7 +72,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 # ── CORS ─────────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://trackspendings.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
